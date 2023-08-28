237df4826a7f1fe4d00faad606c90fc264678d94
hkq-hkq <69070689+hkq-hkq@users.noreply.github.com>
2023-08-28 12:37:36 +0800

    修改顶部滚动条

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,12 +1,11 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
 
-  SITE_CREATE_TIME: '2023-08-28', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2023-09-01', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客' },
   ],
 
   // 英雄区(首页顶部大卡)
